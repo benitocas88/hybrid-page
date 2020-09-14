259533470ca5c710d833e4a92c98d08f37a5df37
Ebenezer Pool Castillo <beni522@gmail.com>
2020-09-14 02:53:05 -0500

    fetching users from dummy api

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import fetch from "node-fetch";
+import {
+  arrayOf,
+  shape
+} from "prop-types";
 
-function index() {
-  return <span>Hello world</span>;
+
+function Index(props) {
+  const { data } = props;
+  return (
+    <ul>
+      {data.map(item => (
+        <li key={item.id}>{ item.email }</li>
+      ))}
+    </ul>
+  );
 }
 
-export default index;
+Index.propTypes = {
+  data: arrayOf(shape()).isRequired
+}
+
+export default Index;
+
+
+export const getServerSideProps = async () => {
+  const request = await fetch("https://reqres.in/api/users?page=1");
+  const response = await request.json()
+  return { props: response }
+}
